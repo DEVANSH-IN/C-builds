1bcb7cab72a4b8193fdd170e7c04157dbb48998d
Devansh Tripathi <devansht266@gmail.com>
2022-12-14 09:11:39 -0500

    modified percentage.c file

--- percentage_marks.c
+++ percentage_marks.c
@@ -2,12 +2,24 @@
 #include<stdio.h>
 int main()
 {
-	int m1,m2,m3,m4,m5,aggr;
-	float per;
-	Printf("\n Enter the marks in 5 subject:-");
-	scanf("%d%d%d%d%d", &m1,&m2,&m3,&m4,&m5);
-	aggr = m1+m2+m3+m4+m5;
-	per = aggr/5;
-	
+    int sub1,sub2,sub3,sub4,sub5, aggr;
+    float per;
+    printf("\n Enter the marks in Subject 1:-");
+    scanf("%d", &sub1);
+     printf("\nEnter the marks in Subject 2:-");
+    scanf("%d",&sub2);
+     printf("\n Enter the marks in Subject 3:-");
+    scanf("%d", &sub3);
+     printf("\n Enter the marks in Subject 4:-");
+    scanf("%d", &sub4);
+     printf("\n Enter the marks in Subject 5:-");
+    scanf("%d", &sub5);
+    aggr = sub1+sub2+sub3+sub4+sub5;
+    per  = aggr/5;
+    printf("Aggregate Marks = %d\n",aggr);
+    printf("Percentage of Marks = %f\n", per,"%");
     return 0;
- }
+     
+    
+    
+}
